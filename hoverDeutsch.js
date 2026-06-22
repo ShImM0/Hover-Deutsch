43697baa92ac6d5610b7844bf9f88d7281a615bd
ShImM0 <zhangcristina333@gmail.com>
2026-06-22 21:10:29 +0200

    Beginning with the word parsing function in JavaScript

---
+++ hoverDeutsch.js
@@ -0,0 +1,40 @@
+
+// Initialise variables
+let word;
+
+/*
+ *  The caretPositionFromPoint() method returns an object, containing the DOM node, the caret and caret's character offset within the node
+ *  The caretRangeFromPoint() method does a similar thing
+ */
+function getWord(e) {
+  let range;
+  let textNode;
+
+  if (document.caretPositionFromPoint) {
+    range = document.caretPositionFromPoint(e.clientX, e.clientY);
+    textNode = range.offsetNode;
+  }
+  else if (document.caretRangeFromPoint) {
+    range = document.caretRangeFromPoint(e.clientX, e.clientY);
+    textNode = range.startContainer;
+  }
+  else {
+    return;
+  }
+
+  if (textNode?.nodeType === 3) {
+    // Only if the type of the node is text (.nodeType == 3)
+    word = textNode;
+    console.log('Parsed: ${word}');
+  }
+}
+
+function getMeaningOfWord(word) {
+
+}
+
+function displayToolTipText(toolTipText) {
+
+}
+
+
