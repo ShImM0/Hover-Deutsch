8f04fc74ce05c502a090bedb3aaa216164bd2dce
ShImM0 <zhangcristina333@gmail.com>
2026-06-23 23:18:00 +0200

    Added checks for word parsing

--- hoverDeutsch.js
+++ hoverDeutsch.js
@@ -1,13 +1,17 @@
-console.log("HoverDeutsch cargado");
-alert("HoverDeutsch alerta de carga");
+
 // Initialise variables
-let word;
+let lastWord = "";
 let toolTipText;
 
 document.addEventListener("mousemove", process);
 
 function process(e) {
-  getWord(e);
+  const word = getWord(e);
+
+  if (word !== undefined && word != lastWord){
+    console.log(word);
+    lastWord = word;
+  }
   //getMeaningOfWord(word);
   //displayToolTipText(toolTipText);
 }
@@ -18,33 +22,56 @@ function process(e) {
 
 
 function getWord(e) {
+  
   let range;
   let textNode;
+  let offset;
 
   if (document.caretPositionFromPoint) {
     range = document.caretPositionFromPoint(e.clientX, e.clientY);
-    
+
     if (!range) return;
     textNode = range.offsetNode;
+    offset = range.offset;
   }
   else if (document.caretRangeFromPoint) {
     range = document.caretRangeFromPoint(e.clientX, e.clientY);
 
     if (!range) return;
     textNode = range.startContainer;
+    offset = range.startOffset;
   }
   else {
     return;
   }
 
-  if (textNode?.nodeType === 3) {
+  if (textNode?.nodeType !== Node.TEXT_NODE) return;
     // Only if the type of the node is text (.nodeType == 3)
-    word = textNode.textContent;
-    console.log(`Parsed: ${word}`);
-  }
-  else {
-    console.log("no es texto");
-  }
+
+    const text = textNode.textContent;
+   
+
+    // /\p{L}/u recognizes any letter
+    // If current letter is undefined or not a letter or is blank, return
+    if (!text[offset] || !/\p{L}/u.test(text[offset])) { 
+      return;
+    }
+
+     let start = offset;
+    let end = offset;
+
+    // Correct the indices for proper delimitation
+    while (start > 0 && /\p{L}/u.test(text[start - 1])) {
+      start--;
+    }
+
+    while (end < text.length && /\p{L}/u.test(text[end])) {
+      end++;
+    }
+
+    // Remove blank spaces after the start and the end
+    return text.slice(start, end);
+
 }
 
 function getMeaningOfWord(word) {
